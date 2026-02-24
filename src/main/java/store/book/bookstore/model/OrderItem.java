712eb93a8da7ed4d358ce7b9198048d894eb8558
MYKOLA YAKYMCHUK <m.yakymchuk5@gmail.com>
2026-02-25 00:48:46 +0100

    replace @Data with @Getter @Setter on all entity classes

--- src/main/java/store/book/bookstore/model/OrderItem.java
+++ src/main/java/store/book/bookstore/model/OrderItem.java
@@ -10,7 +10,6 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.math.BigDecimal;
-
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
