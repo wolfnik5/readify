4ec0ba8786cff60aa09915c111f8e0d4aa4f36c0
MYKOLA YAKYMCHUK <m.yakymchuk5@gmail.com>
2026-03-14 10:59:40 +0100

    added @UtilityClass to the TestDataHelper, changed variable names

--- src/test/java/store/book/bookstore/util/TestDataHelper.java
+++ src/test/java/store/book/bookstore/util/TestDataHelper.java
@@ -2,6 +2,7 @@ package store.book.bookstore.util;
 
 import java.math.BigDecimal;
 import java.util.Set;
+import lombok.experimental.UtilityClass;
 import store.book.bookstore.dto.BookDto;
 import store.book.bookstore.dto.BookDtoWithoutCategoryIds;
 import store.book.bookstore.dto.CategoryDto;
@@ -10,63 +11,64 @@ import store.book.bookstore.dto.CreateBookRequestDto;
 import store.book.bookstore.model.Book;
 import store.book.bookstore.model.Category;
 
+@UtilityClass
 public class TestDataHelper {
 
-    public static final String BOOK_TITLE = "Warriors: Into the Wild";
-    public static final String BOOK_AUTHOR = "Erin Hunter";
-    public static final String BOOK_ISBN = "9780329373528";
-    public static final BigDecimal BOOK_PRICE = BigDecimal.valueOf(29.99);
+    public final String BOOK_TITLE = "Warriors: Into the Wild";
+    public final String BOOK_AUTHOR = "Erin Hunter";
+    public final String BOOK_ISBN = "9780329373528";
+    public final BigDecimal BOOK_PRICE = BigDecimal.valueOf(29.99);
 
-    public static final String BOOK_2_TITLE = "Effective Java";
-    public static final String BOOK_2_AUTHOR = "Joshua Bloch";
-    public static final String BOOK_2_ISBN = "9780134685991";
-    public static final BigDecimal BOOK_2_PRICE = BigDecimal.valueOf(39.99);
+    public final String BOOK_SECOND_TITLE = "Effective Java";
+    public final String BOOK_SECOND_AUTHOR = "Joshua Bloch";
+    public final String BOOK_SECOND_ISBN = "9780134685991";
+    public final BigDecimal BOOK_SECOND_PRICE = BigDecimal.valueOf(39.99);
 
-    public static final String CATEGORY_NAME = "Fantasy novel";
-    public static final String CATEGORY_DESCRIPTION =
+    public final String CATEGORY_NAME = "Fantasy novel";
+    public final String CATEGORY_DESCRIPTION =
             "Fiction featuring magical or supernatural elements";
 
-    public static final String CATEGORY_2_NAME = "Science Fiction";
-    public static final String CATEGORY_2_DESCRIPTION =
+    public final String CATEGORY_SECOND_NAME = "Science Fiction";
+    public final String CATEGORY_SECOND_DESCRIPTION =
             "Sci-fi books";
 
-    public static final String CATEGORY_3_NAME = "Horror";
-    public static final String CATEGORY_3_DESCRIPTION =
+    public final String CATEGORY_THIRD_NAME = "Horror";
+    public final String CATEGORY_THIRD_DESCRIPTION =
             "Horror books";
 
-    public static final String ADMIN_EMAIL = "admin@bookstore.com";
-    public static final String ADMIN_PASSWORD = "admin1234";
-    public static final String USER_EMAIL = "user@bookstore.com";
-    public static final String USER_PASSWORD = "user1234";
+    public final String ADMIN_EMAIL = "admin@bookstore.com";
+    public final String ADMIN_PASSWORD = "admin1234";
+    public final String USER_EMAIL = "user@bookstore.com";
+    public final String USER_PASSWORD = "user1234";
 
-    public static Category buildCategory() {
+    public Category buildCategory() {
         Category category = new Category();
         category.setName(CATEGORY_NAME);
         category.setDescription(CATEGORY_DESCRIPTION);
         return category;
     }
 
-    public static Category buildCategory(Long id) {
+    public Category buildCategory(Long id) {
         Category category = buildCategory();
         category.setId(id);
         return category;
     }
 
-    public static Category buildCategory2() {
+    public Category buildCategory2() {
         Category category = new Category();
-        category.setName(CATEGORY_2_NAME);
-        category.setDescription(CATEGORY_2_DESCRIPTION);
+        category.setName(CATEGORY_SECOND_NAME);
+        category.setDescription(CATEGORY_SECOND_DESCRIPTION);
         return category;
     }
 
-    public static Category buildCategory3() {
+    public Category buildCategory3() {
         Category category = new Category();
-        category.setName(CATEGORY_3_NAME);
-        category.setDescription(CATEGORY_3_DESCRIPTION);
+        category.setName(CATEGORY_THIRD_NAME);
+        category.setDescription(CATEGORY_THIRD_DESCRIPTION);
         return category;
     }
 
-    public static Book buildBook(Category category) {
+    public Book buildBook(Category category) {
         Book book = new Book();
         book.setTitle(BOOK_TITLE);
         book.setAuthor(BOOK_AUTHOR);
@@ -76,23 +78,23 @@ public class TestDataHelper {
         return book;
     }
 
-    public static Book buildBook(Long id, Category category) {
+    public Book buildBook(Long id, Category category) {
         Book book = buildBook(category);
         book.setId(id);
         return book;
     }
 
-    public static Book buildBook2(Category category) {
+    public Book buildBook2(Category category) {
         Book book = new Book();
-        book.setTitle(BOOK_2_TITLE);
-        book.setAuthor(BOOK_2_AUTHOR);
-        book.setIsbn(BOOK_2_ISBN);
-        book.setPrice(BOOK_2_PRICE);
+        book.setTitle(BOOK_SECOND_TITLE);
+        book.setAuthor(BOOK_SECOND_AUTHOR);
+        book.setIsbn(BOOK_SECOND_ISBN);
+        book.setPrice(BOOK_SECOND_PRICE);
         book.setCategories(Set.of(category));
         return book;
     }
 
-    public static CreateBookRequestDto buildCreateBookRequest(Long categoryId) {
+    public CreateBookRequestDto buildCreateBookRequest(Long categoryId) {
         CreateBookRequestDto req = new CreateBookRequestDto();
         req.setTitle(BOOK_TITLE);
         req.setAuthor(BOOK_AUTHOR);
@@ -102,7 +104,7 @@ public class TestDataHelper {
         return req;
     }
 
-    public static BookDto buildBookDto(Long id, Long categoryId) {
+    public BookDto buildBookDto(Long id, Long categoryId) {
         BookDto dto = new BookDto();
         dto.setId(id);
         dto.setTitle(BOOK_TITLE);
@@ -113,7 +115,7 @@ public class TestDataHelper {
         return dto;
     }
 
-    public static BookDtoWithoutCategoryIds buildBookDtoWithoutCategories(Long id) {
+    public BookDtoWithoutCategoryIds buildBookDtoWithoutCategories(Long id) {
         BookDtoWithoutCategoryIds dto = new BookDtoWithoutCategoryIds();
         dto.setId(id);
         dto.setTitle(BOOK_TITLE);
@@ -123,11 +125,11 @@ public class TestDataHelper {
         return dto;
     }
 
-    public static CategoryRequestDto buildCategoryRequest() {
+    public CategoryRequestDto buildCategoryRequest() {
         return new CategoryRequestDto(CATEGORY_NAME, CATEGORY_DESCRIPTION);
     }
 
-    public static CategoryDto buildCategoryDto(Long id) {
+    public CategoryDto buildCategoryDto(Long id) {
         return new CategoryDto(id, CATEGORY_NAME, CATEGORY_DESCRIPTION);
     }
 }
